a7b0cb4ba40472e9caf48abef1b5604124d87e27
Joseph Kao <josephtkao@gmail.com>
2020-04-16 00:41:48 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,4 +9,12 @@ class Author
     Post.all.select {|posts| posts.author == self}
   end
 
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
 end
